ddbb1a2c5433cae063663c44292d857654aae83c
Bashlychev <gbashlychev@gmail.com>
2022-11-29 15:30:34 +0500

    git push

---
+++ src/SearchInArray.java
@@ -0,0 +1,28 @@
+ public class SearchInArray {
+     public static void main(String[] args) {
+         int[] arraySmall = {20, 91, 64, 75, 19, 26, 48, 22, 50};
+         printMaximumElement(arraySmall);
+
+     }
+
+     static void printMaximumElement(int[] array) {
+         int max = array[0];
+         int maxIndex = 0;
+         int comparisons = 0;
+         int replacements = 0;
+         for (int i = 0; i < array.length; i++) {
+             if (array[i] > max) {
+                 max = array[i];
+                 maxIndex = i;
+                 replacements++;
+             }
+             comparisons++;
+         }
+         System.out.println("Max element: " + max);
+         System.out.println("Max element index: " + maxIndex);
+         System.out.println("Comparisons: " + comparisons);
+         System.out.println("Replacements: " + replacements);
+         {
+         }
+     }
+ }
